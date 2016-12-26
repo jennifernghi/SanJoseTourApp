709ac71dd6166f75a701f73c449d3ceefaccb5d3
jennifernghinguyen <jennifernghi@yahoo.com>
2016-12-25 22:23:36 -0800

    Restaurant fragment

--- app/src/main/java/com/example/android/sanjosetourapp/RestaurantFragment.java
+++ app/src/main/java/com/example/android/sanjosetourapp/RestaurantFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,17 +17,31 @@ import android.widget.TextView;
 public class RestaurantFragment extends Fragment {
 
 
-    public RestaurantFragment() {
-        // Required empty public constructor
-    }
 
+    public RestaurantFragment() {
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
-    }
+        View rootView = inflater.inflate(R.layout.list_view, container,false);
+        final ArrayList<Location> locations = new ArrayList<>();
+        locations.add(new Restaurant(getString(R.string.kali_restaurant_name), getString(R.string.kali_restaurant_address), getString(R.string.kali_restaurant_description), R.drawable.kali_restaurant));
+        locations.add(new Restaurant(getString(R.string.cubamex_restaurant_name), getString(R.string.cubamex_restaurant_address), getString(R.string.cubamex_restaurant_description), R.drawable.cubamex));
+        locations.add(new Restaurant(getString(R.string.the_city_fish_restaurant_name), getString(R.string.the_city_fish_restaurant_address), getString(R.string.the_city_fish_restaurant_description), R.drawable.the_city_fish));
+        locations.add(new Restaurant(getString(R.string.danbi_korean_restaurant_name), getString(R.string.danbi_korean_restaurant_address), getString(R.string.danbi_korean_restaurant_description), R.drawable.danbi_korean_restaurant));
+        locations.add(new Restaurant(getString(R.string.hawaiian_poke_bowl_restaurant_name), getString(R.string.hawaiian_poke_bowl_restaurant_address), getString(R.string.hawaiian_poke_bowl_restaurant_description), R.drawable.hawaiian_poke_bowl));
 
+
+        LocationAdapter adapter = new LocationAdapter(getActivity(), locations);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+        listView.setAdapter(adapter);
+        return rootView;
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
 }
