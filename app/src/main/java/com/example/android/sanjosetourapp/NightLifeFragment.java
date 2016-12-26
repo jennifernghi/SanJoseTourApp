03356a0a4f3e23fccbe21b6bea40eaa01ee8f36e
jennifernghinguyen <jennifernghi@yahoo.com>
2016-12-25 23:25:57 -0800

    Nightlife fragment

--- app/src/main/java/com/example/android/sanjosetourapp/NightLifeFragment.java
+++ app/src/main/java/com/example/android/sanjosetourapp/NightLifeFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +25,23 @@ public class NightLifeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View rootView = inflater.inflate(R.layout.list_view, container,false);
+        final ArrayList<Location> locations = new ArrayList<>();
+        locations.add(new NightLife(getString(R.string.edgie_billiards_name), getString(R.string.edgie_billiards_address), getString(R.string.edgie_billiards_description), R.drawable.edgie_billiards));
+        locations.add(new NightLife(getString(R.string.comedysportz_name), getString(R.string.comedysportz_address), getString(R.string.comedysportz_description), R.drawable.comedysportz));
+        locations.add(new NightLife(getString(R.string.south_bay_apa_name), getString(R.string.south_bay_apa_address), getString(R.string.south_bay_apa_description)));
+        locations.add(new NightLife(getString(R.string.stage_4_improv_name), getString(R.string.stage_4_improv_address), getString(R.string.stage_4_improv_description), R.drawable.stage_4_improv));
+
+
+        LocationAdapter adapter = new LocationAdapter(getActivity(), locations);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+        listView.setAdapter(adapter);
+        return rootView;
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
     }
 
 }
