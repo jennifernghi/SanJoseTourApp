5831b62143887b1ce7b5d46b44c0189049d6d012
jennifernghinguyen <jennifernghi@yahoo.com>
2016-12-25 18:24:25 -0800

    add tab

--- app/src/main/java/com/example/android/sanjosetourapp/MainActivity.java
+++ app/src/main/java/com/example/android/sanjosetourapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.sanjosetourapp;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- app/src/main/java/com/example/android/sanjosetourapp/ViewPagerAdapter.java
+++ app/src/main/java/com/example/android/sanjosetourapp/ViewPagerAdapter.java
@@ -13,7 +13,8 @@ import java.util.List;
  */
 public class ViewPagerAdapter extends FragmentPagerAdapter {
 
-    private String[] pageTitle = {String.valueOf(R.string.restaurant_category),String.valueOf(R.string.night_life_category),String.valueOf(R.string.shopping_category),String.valueOf(R.string.nature_category)};
+    //private String[] pageTitle = {String.valueOf(R.string.restaurant_category),String.valueOf(R.string.night_life_category),String.valueOf(R.string.shopping_category),String.valueOf(R.string.nature_category)};
+    private String[] pageTitle = {"Restaurant","Nightlife","Shopping","Nature"};
 
     public ViewPagerAdapter(FragmentManager fm) {
         super(fm);
@@ -21,11 +22,32 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+       if(position==0){
+           return new RestaurantFragment();
+       }else if(position==1){
+           return new NightLifeFragment();
+       }else if (position==2){
+           return new ShoppingFragment();
+       }else{
+           return new NatureFragment();
+       }
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return pageTitle.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if(position==0){
+            return "Restaurant";
+        }else if(position==1){
+            return "Nightlife";
+        }else if (position==2){
+            return "Shopping";
+        }else{
+            return "Nature";
+        }
     }
 }
