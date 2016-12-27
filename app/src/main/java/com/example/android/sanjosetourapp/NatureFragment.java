e045a5ce7c64b335bc5f7a903dc57d8eb3262188
jennifernghinguyen <jennifernghi@yahoo.com>
2016-12-26 20:41:28 -0800

    nature fragment

--- app/src/main/java/com/example/android/sanjosetourapp/NatureFragment.java
+++ app/src/main/java/com/example/android/sanjosetourapp/NatureFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +25,24 @@ public class NatureFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View rootView = inflater.inflate(R.layout.list_view, container,false);
+        final ArrayList<Location> locations = new ArrayList<>();
+        locations.add(new Nature(getString(R.string.sierra_vista_open_space_preserve_name), getString(R.string.sierra_vista_open_space_preserve_address), getString(R.string.sierra_vista_open_space_preserve_description), R.drawable.sierra_vista_open_space_preserve));
+        locations.add(new Nature(getString(R.string.sierra_road_name), getString(R.string.sierra_road_address), getString(R.string.sierra_road_description)));
+        locations.add(new Nature(getString(R.string.mission_peak_regional_park_name), getString(R.string.mission_peak_regional_park_address), getString(R.string.mission_peak_regional_park_description)));
+        locations.add(new Nature(getString(R.string.san_jose_municipal_rose_garden_name), getString(R.string.san_jose_municipal_rose_garden_address), getString(R.string.san_jose_municipal_rose_garden_description), R.drawable.san_jose_municipal_rose_garden));
+        locations.add(new Nature(getString(R.string.japanese_friendship_garden_name), getString(R.string.japanese_friendship_garden_address), getString(R.string.japanese_friendship_garden_description), R.drawable.japanese_friendship_garden));
+
+
+        LocationAdapter adapter = new LocationAdapter(getActivity(), locations);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+        listView.setAdapter(adapter);
+        return rootView;
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
     }
 
 }
