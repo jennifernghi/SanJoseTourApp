56ee18eef20663f344329e891ffdce3c7bbae69b
jennifernghinguyen <jennifernghi@yahoo.com>
2016-12-26 20:13:42 -0800

    shopping fragment

--- app/src/main/java/com/example/android/sanjosetourapp/ShoppingFragment.java
+++ app/src/main/java/com/example/android/sanjosetourapp/ShoppingFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +25,24 @@ public class ShoppingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View rootView = inflater.inflate(R.layout.list_view, container,false);
+        final ArrayList<Location> locations = new ArrayList<>();
+        locations.add(new Shopping(getString(R.string.santana_row_name), getString(R.string.santana_row_address), getString(R.string.santana_row_description), R.drawable.santana_row));
+        locations.add(new Shopping(getString(R.string.eastridge_name), getString(R.string.eastridge_address), getString(R.string.eastridge_description), R.drawable.eastridge));
+        locations.add(new Shopping(getString(R.string.west_field_valley_fair_name), getString(R.string.west_field_valley_fair_address), getString(R.string.west_field_valley_fair_description), R.drawable.westfield));
+        locations.add(new Shopping(getString(R.string.west_field_oakridge_name), getString(R.string.west_field_oakridge_address), getString(R.string.west_field_oakridge_description)));
+        locations.add(new Shopping(getString(R.string.downtown_willow_glen_name), getString(R.string.downtown_willow_glen_address), getString(R.string.downtown_willow_glen_description)));
+
+
+        LocationAdapter adapter = new LocationAdapter(getActivity(), locations);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+        listView.setAdapter(adapter);
+        return rootView;
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
     }
 
 }
